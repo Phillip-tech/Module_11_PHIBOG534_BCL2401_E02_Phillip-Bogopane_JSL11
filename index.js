2980a87330034cdf1c6480600bd75327d42ee9ad
Phillip Bogopane <pm.bogopane@gamil.com>
2024-04-25 13:22:36 +0200

    Updated readme file

--- index.js
+++ index.js
@@ -89,14 +89,6 @@ function displayBoards(boards) {
   });
 }
 
-// Filters tasks corresponding to the board name and displays them on the DOM.
-// TASK: Fix Bugs
-function filterAndDisplayTasksByBoard(boardName) {
-  const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
-
-  // Ensure the column titles are set outside of this function or correctly initialized before this function runs
-
  // Filters tasks corresponding to the board name and displays them on the DOM.
 function filterAndDisplayTasksByBoard(boardName) {
   const tasks = getTasks();
