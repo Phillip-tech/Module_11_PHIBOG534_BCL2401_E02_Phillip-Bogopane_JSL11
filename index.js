8cdf05281959c2dc93ee7fd5d010086f456a16c7
Phillip Bogopane <pm.bogopane@gamil.com>
2024-04-25 11:07:48 +0200

    Fixed : Function checks if local storage already has data

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ import { initialData } from './initialData.js';
 // Function checks if local storage already has data, if not it loads initialData to localStorage
 function initializeData() {
   if (!localStorage.getItem('tasks')) {
-    localStorage.setItem('tasks', JSON.stringify(initialData)); 
-    localStorage.setItem('showSideBar', 'true')
+      if (typeof initialData === 'object' && initialData !== null) {
+          localStorage.setItem('tasks', JSON.stringify(initialData));
+          localStorage.setItem('showSideBar', 'true');
+      } else {
+          console.error('Invalid initialData');
+      }
   } else {
-    console.log('Data already exists in localStorage');
+      console.log('Data already exists in localStorage');
   }
 }
+
 
 // TASK: Get elements from the DOM
 const elements = {
